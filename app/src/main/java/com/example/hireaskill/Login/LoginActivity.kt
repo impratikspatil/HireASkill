91ef3d832ab69aab2e9a125dadbdd0c1c3fcc565
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2022-02-19 23:44:00 +0530

    added login background

---
+++ app/src/main/java/com/example/hireaskill/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hireaskill.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hireaskill.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
