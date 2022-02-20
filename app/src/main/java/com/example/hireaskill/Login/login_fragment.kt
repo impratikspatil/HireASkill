e2385deaa5f433f86394c72a46b431033811314a
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2022-02-20 15:52:17 +0530

    Login UI Done

--- app/src/main/java/com/example/hireaskill/Login/login_fragment.kt
+++ app/src/main/java/com/example/hireaskill/Login/login_fragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.hireaskill.R
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.hireaskill.databinding.FragmentLoginFragmentBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [login_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class login_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding :FragmentLoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_fragment, container, false)
+    ): View {
+        binding = FragmentLoginFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment login_fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            login_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.registerNow.setOnClickListener{
+            Toast.makeText(context,"REGISTER_NOW",Toast.LENGTH_SHORT).show()
+        }
+        binding.button.setOnClickListener{
+            Toast.makeText(context,"LOGIN",Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
