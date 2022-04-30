3c75dc6da14cfefc93855899b6f8afd994159da5
techtalks-crypto <impratikspatil@gmail.com>
2022-04-30 11:17:43 +0530

    FIREBASE FOR JOB ADDED

--- app/src/main/java/com/example/hireaskill/Home/Add_Job_fragment.kt
+++ app/src/main/java/com/example/hireaskill/Home/Add_Job_fragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.hireaskill.R
-
+import com.example.hireaskill.databinding.FragmentAddJobFragmentBinding
+import com.example.hireaskill.databinding.FragmentLoginFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class Add_Job_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+
+    private lateinit var binding:FragmentAddJobFragmentBinding
+    private lateinit var database:DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +33,42 @@ class Add_Job_fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add__job_fragment, container, false)
+        binding = FragmentAddJobFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addJobsBtn.setOnClickListener {
+            val userid_forjob = FirebaseAuth.getInstance().currentUser
+            val username=binding.txtName.text.toString()
+            val Job_Title=binding.txtjobtitle.text.toString()
+            val Location=binding.txtlocation.text.toString()
+            val Salary=binding.salary.text.toString()
+            val Description=binding.txtrequirements.text.toString()
+
+            database=FirebaseDatabase.getInstance().getReference("username")
+            val userJob=UserJob(username,Job_Title,Location,Salary,Description)
+            database.child(userid_forjob.toString()).setValue(userJob).addOnSuccessListener {
+                binding.txtName.text?.clear()
+                binding.txtjobtitle.text?.clear()
+                binding.txtlocation.text?.clear()
+                binding.salary.text?.clear()
+                binding.txtrequirements.text?.clear()
+
+
+
+
+            }.addOnFailureListener {
+
+
+
+            }
+
+        }
+
+
     }
 
  
---
+++ app/src/main/java/com/example/hireaskill/Home/UserJob.kt
@@ -0,0 +1,3 @@
+package com.example.hireaskill.Home
+
+data class UserJob(val username:String?=null, val jobt:String?=null,val jobloc:String?=null,val jobsal:String?=null,val jobdes:String?=null,)
