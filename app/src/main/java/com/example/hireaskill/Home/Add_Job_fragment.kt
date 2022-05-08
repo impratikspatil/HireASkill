4eaa524ff433ea47c830d25a415b23943b144ef5
Mrunmayii <shirke.mrunmayi@gmail.com>
2022-05-08 14:32:14 +0530

    Chat incomplete

--- app/src/main/java/com/example/hireaskill/Home/Add_Job_fragment.kt
+++ app/src/main/java/com/example/hireaskill/Home/Add_Job_fragment.kt
@@ -20,8 +20,8 @@ import com.google.firebase.database.FirebaseDatabase
 class Add_Job_fragment : Fragment() {
 
 
-    private lateinit var binding:FragmentAddJobFragmentBinding
-    private lateinit var database:DatabaseReference
+    private lateinit var binding: FragmentAddJobFragmentBinding
+    private lateinit var database: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,31 +49,24 @@ class Add_Job_fragment : Fragment() {
 //        bottomNavigationView.visibility = View.GONE
 
 
-
         binding.addJobsBtn.setOnClickListener {
 
-            if(binding.txtName.text?.isEmpty() == true){
-                binding.txtName.error="Username can not be empty!"
+            if (binding.txtName.text?.isEmpty() == true) {
+                binding.txtName.error = "Username can not be empty!"
                 binding.txtName.requestFocus()
-            }
-            else if(binding.txtjobtitle.text.toString().isEmpty()){
-                binding.txtjobtitle.error="Job title can not be empty!"
+            } else if (binding.txtjobtitle.text.toString().isEmpty()) {
+                binding.txtjobtitle.error = "Job title can not be empty!"
                 binding.txtjobtitle.requestFocus()
-            }
-            else if(binding.txtlocation.text.toString().isEmpty()){
-                binding.txtlocation.error="Location can not be empty!"
+            } else if (binding.txtlocation.text.toString().isEmpty()) {
+                binding.txtlocation.error = "Location can not be empty!"
                 binding.txtlocation.requestFocus()
-            }
-            else if(binding.txtrequirements.text.toString().isEmpty()){
-                binding.txtrequirements.error="Requirements can not be empty!"
+            } else if (binding.txtrequirements.text.toString().isEmpty()) {
+                binding.txtrequirements.error = "Requirements can not be empty!"
                 binding.txtrequirements.requestFocus()
-            }
-            else if(binding.salary.text.toString().isEmpty()){
-                binding.salary.error="Salary can not be empty!"
+            } else if (binding.salary.text.toString().isEmpty()) {
+                binding.salary.error = "Salary can not be empty!"
                 binding.salary.requestFocus()
-            }
-
-            else {
+            } else {
 
                 val userid_forjob = FirebaseAuth.getInstance().currentUser?.uid
                 val username = binding.txtName.text.toString()
@@ -82,32 +75,39 @@ class Add_Job_fragment : Fragment() {
                 val Salary = binding.salary.text.toString()
                 val Description = binding.txtrequirements.text.toString()
 
-            database=FirebaseDatabase.getInstance().getReference("Jobs")
-            val userJob=UserJob(Job_Title,Location,Salary,Description,username,userid_forjob)
-            val hash = HashMap<String,Any>()
-            hash.put(Job_Title,userJob)
-            //hash[Job_Title] = userJob
-            if (userid_forjob != null) {
-                database.child(userid_forjob).updateChildren(hash).addOnSuccessListener {
-                    binding.txtName.text?.clear()
-                    binding.txtjobtitle.text?.clear()
-                    binding.txtlocation.text?.clear()
-                    binding.salary.text?.clear()
-                    binding.txtrequirements.text?.clear()
+                database = FirebaseDatabase.getInstance().getReference("Jobs")
+                val userJob =
+                    UserJob(Job_Title, Location, Salary, Description, username, userid_forjob)
+                val hash = HashMap<String, Any>()
+                hash.put(Job_Title, userJob)
+                //hash[Job_Title] = userJob
+                if (userid_forjob != null) {
+                    database.child(userid_forjob).updateChildren(hash).addOnSuccessListener {
+                        binding.txtName.text?.clear()
+                        binding.txtjobtitle.text?.clear()
+                        binding.txtlocation.text?.clear()
+                        binding.salary.text?.clear()
+                        binding.txtrequirements.text?.clear()
 
-                    Toast.makeText(context, "INFORMATION UPDATED SUCCESSFULLY ", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context,
+                            "INFORMATION UPDATED SUCCESSFULLY ",
+                            Toast.LENGTH_SHORT
+                        ).show()
 
-                }.addOnFailureListener {
+                    }.addOnFailureListener {
 
-                    Toast.makeText(context, "INFORMATION FAILED TO UPDATE ", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, "INFORMATION FAILED TO UPDATE ", Toast.LENGTH_SHORT)
+                            .show()
 
+                    }
                 }
+
             }
+
 
         }
 
 
     }
-
- 
 }
